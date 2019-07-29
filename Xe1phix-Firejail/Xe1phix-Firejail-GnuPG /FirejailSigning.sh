27cf10812f1cfc8b51eb78038aabf739492eb7f6
Xe1phix <xe1phix@protonmail.ch>
2019-07-29 07:04:22 +0000

    Upload New File

---
+++ Xe1phix-Firejail/Xe1phix-Firejail-GnuPG /FirejailSigning.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+## FirejailSigning.sh
+
+echo "Calculationg SHA256 for all files in /transfer - firejail version $1"
+
+cd /transfer
+sha256sum * > firejail-$1-unsigned
+gpg --clearsign --digest-algo SHA256 < firejail-$1-unsigned > firejail-$1.asc
+gpg --verify firejail-$1.asc
+gpg --detach-sign --armor firejail-$1.tar.xz
