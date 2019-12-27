d7f33bb71c29bdef6d0b0b1fd698009c8e0739c7
Xe1phix <xe1phix@protonmail.ch>
2019-12-27 14:31:23 +0000

    Upload New File

---
+++ Xe1phix-InfoSec-Links/Xe1phix-Resource-Lists+Other-Stuff-v5.8.sh
@@ -0,0 +1,179 @@
+
+
+Search engines
+
+    Searx.me − a hackable FOSS meta search engine.
+    Searx mirror list − health report of public Searx mirrors.
+    Startpage − a search engine powered by Google search that doesn't track you.
+    DuckDuckGo − trust me, I'm a duck.
+    Pipl.com − a search engine that helps you find user accounts of people.
+    Shodan − a search engine for hackers that can help find devices connected to the internet.
+    Sharedir − a search engine for files uploaded to file hosting sites.
+    Torrentz² − a meta search engine for torrents.
+
+
+
+
+Anonymity checkers
+
+    ipleak.net − basic anonymity tests.
+    DNS leak test
+    Whoer.net − extended anonymity tests.
+    Browser fingerprinting test
+    Cache tracking checker
+    Browserleaks.com − a whole range of tests using various fingerprinting techniques.
+    ghacks test list − the ultimate online privacy test resource list.
+    FillDisk − an HTML5 exploit that fills your disk with pictures of cats (does not work on Firefox-based browsers).
+
+
+
+     
+
+Fingerprinting tests
+
+    BrowserSpy.dk
+    BrowserLeaks.com
+    AmIUnique [1]
+    Panopticlick
+    Unique Machine
+    Firefox Addon Detector [1]
+    AudioContext Fingerprint Test Page
+    Evercookie
+    WebRTC Test Landing Page
+    getUserMedia Test Page
+    Onion test for CORS and WebSocket
+    Official WebGL check
+    WebGL Report
+    Battery API [1]
+    WebRTC LAN address leak test
+    IP Check
+    OONI Internet censorship tests
+
+SSL tests
+
+    SSL Client Test
+    How's My SSL
+    Mixed content tests (Mozilla)
+    Mixed content tests (Microsoft)
+    SSL Checker | Symantec CryptoReport
+    Bad SSL
+
+
+
+https://github.com/log2timeline/plaso
+https://github.com/pyllyukko/harden.sh
+https://github.com/pyllyukko/swsp
+
+
+
+
+
+
+
+
+
+
+---------------
+ Moar-Backups
+---------------
+
+moz-extension://e98d10ae-65fb-4a74-8e44-0733530b3423/logger-ui.html
+https://boards.4chan.org/gif/thread/16113247
+https://boards.4chan.org/b/catalog
+https://www.digi77.com/linux-kodachi/
+https://www.unixmen.com/setting-dns-server-centos-7/
+https://www.whonix.org/wiki/Dev/Build_Documentation/Physical_Isolation#How_To_Install_Whonix-Gateway_.E2.84.A2_on_the_Raspberry_Pi_3_B_.28RPI3.29
+https://distrowatch.com/
+https://boards.4chan.org/gif/thread/16077787#p16079486
+https://www.commandlinefu.com/commands/matching/wget/d2dldA==/sort-by-votes/25/50/75
+https://catonmat.net/top-ten-one-liners-from-commandlinefu-explained
+http://blog.commandlinekungfu.com/
+https://www.youtube.com/watch?v=ACor271BvU8&list=PLFtHsziXsq2dyx1m78pdzfJ7TBlXPk75r&index=2
+https://distrowatch.com/table.php?distribution=blackarch
+https://github.com/search?l=Shell&p=2&q=.qcow2&type=Repositories
+https://github.com/juanminm/script-QEMU-KVM
+https://github.com/ProtonVPN/protonvpn-cli-ng/blob/master/README.md
+https://github.com/ProtonVPN/protonvpn-cli-ng/blob/master/USAGE.md
+https://zeltser.com/remnux-malware-analysis-tips/
+https://digital-forensics.sans.org/blog/category/remnux
+https://github.com/reuteras/remnux-tools
+https://www.sans.org/security-resources/posters/sift-remnux-poster/125/download
+https://bitbucket.org/ethanr/honey-badger-open-source/src/master/
+https://github.com/enaqx/awesome-pentest#online-penetration-testing-resources
+https://dnsdumpster.com/
+https://github.com/evilsocket/xray
+https://github.com/0x90/wifi-arsenal
+https://osintframework.com/
+https://github.com/techgaun/dotfiles
+https://github.com/techgaun/awesome-readings
+https://github.com/techgaun/tg-misc-scripts
+https://github.com/techgaun/git-cheat
+https://github.com/techgaun/ctf-tools
+https://github.com/techgaun/notrack
+https://github.com/techgaun/mustang
+https://github.com/techgaun/tg-shell-scripts
+https://gist.github.com/jakewarren/817070277ff36694165d
+https://github.com/search?l=Shell&q=dns+recon&type=Wikis
+https://github.com/1N3/Sn1per/blob/master/osint.sh
+https://github.com/1N3/Sn1per/wiki
+https://github.com/search?p=3&q=snort&type=Repositories
+https://github.com/brianfco/fwsnort.save
+https://github.com/isislovecruft/fwsnort
+https://github.com/mrash/fwsnort
+https://github.com/topics/intrusion-detection
+https://github.com/Bunplugged/Networking/wiki
+https://github.com/vital2/vital-development/wiki/Computer-Networks-Labs
+https://github.com/theodorehadges/virtual_lab/wiki/Introduction-to-Network-Security-Labs
+https://github.com/dsvetlov/lightsiem
+https://github.com/spankowitz/training/wiki/IDS-IPS
+https://github.com/ayushmaskey/ayushmaskey.github.io/wiki/so_security_onion_cmd
+https://github.com/Wanderson304/GSI-Gestao-da-Seguranca-da-Informcao/wiki/Servi%C3%A7o-IDS
+https://github.com/osogama/security-onion/wiki/Sguil
+https://github.com/Security-Onion-Solutions/security-onion
+https://github.com/mcholste/elsa/wiki/Documentation
+https://github.com/rmusser01/MIS/wiki/Setting-up-a-Virtual-NSM-network-on-a-single-Machine
+https://github.com/filinpavel/whitehat/wiki/Blue-Team-Summary
+https://github.com/homelabwiki/homelabwiki
+https://github.com/gabx/thetradinghall/wiki/Hardened
+https://github.com/digitalbond/Quickdraw-Snort
+https://github.com/sous-chefs/snort
+https://github.com/nccgroup/IP-reputation-snort-rule-generator/blob/master/tepig.pl
+https://github.com/nccgroup/port-scan-automation
+https://github.com/nccgroup/Cyber-Defence
+https://github.com/ShotokanZH/BanBOT-TelegramAPI-Bot/blob/master/bot.sh
+https://github.com/KMGbully/situational-awareness/blob/master/situational-awareness.sh
+https://github.com/KMGbully/hacksaw
+https://github.com/tarrinho/dns.recon.crt
+https://github.com/dustinbutterworth/butters-dns-recon
+https://github.com/dafthack/PowerMeta
+moz-extension://5c4fb747-d790-403f-a63f-5997ecc31295/document-blocked.html?details=eyJ1cmwiOiJodHRwczovL2hpZGRlbmlsbHVzaW9uLmJsb2dzcG90LmNvbS8yMDEyLzA2L2dldHRpbmctd2hhdC15b3Utd2FudC1vdXQtb2YtcGRmLXdpdGguaHRtbCIsImhuIjoiaGlkZGVuaWxsdXNpb24uYmxvZ3Nwb3QuY29tIiwiZG4iOiJoaWRkZW5pbGx1c2lvbi5ibG9nc3BvdC5jb20iLCJmYyI6IlswLDYyLFwiYmxvZ3Nwb3QuY29tXCJdIiwiZnMiOiJ8fGJsb2dzcG90LmNvbV4ifQ==
+moz-extension://5c4fb747-d790-403f-a63f-5997ecc31295/document-blocked.html?details=eyJ1cmwiOiJodHRwczovL2RpZ2l0YWwtZm9yZW5zaWNzLXNjaWVuY2UuYmxvZ3Nwb3QuY29tLzIwMTUvMDkvaG93LXRvLWluc3RhbGwtc2lmdC13b3Jrc3RhdGlvbi1hbmQuaHRtbCIsImhuIjoiZGlnaXRhbC1mb3JlbnNpY3Mtc2NpZW5jZS5ibG9nc3BvdC5jb20iLCJkbiI6ImRpZ2l0YWwtZm9yZW5zaWNzLXNjaWVuY2UuYmxvZ3Nwb3QuY29tIiwiZmMiOiJbMCw2MixcImJsb2dzcG90LmNvbVwiXSIsImZzIjoifHxibG9nc3BvdC5jb21eIn0=
+https://paul.is-a-geek.org/2018/02/dns-based-adblock-using-mikrotik-routeros/
+https://www.hirensbootcd.org/download/
+http://trinityhome.org/Home/index.php?content=TRINITY_RESCUE_KIT_DOWNLOAD
+https://github.com/Security-Onion-Solutions/security-onion/wiki/IntroductionToSecurityOnion
+https://securityonion.readthedocs.io/en/latest/
+http://www.convertcsv.com/url-extractor.htm
+https://www.unixmen.com/setting-dns-server-centos-7/
+https://paul.is-a-geek.org/2018/02/dns-based-adblock-using-mikrotik-routeros/
+https://raw.githubusercontent.com/KMGbully/recon-kmg/master/recon-kmg.sh
+https://iptorrents.com/details.php?id=865396&page=1
+https://iptorrents.com/details.php?id=2956442
+https://booteille.gitlab.io/invimatrix/#
+https://raw.githubusercontent.com/uMatrix-Rules/uMatrix-Rules-Site/master/rules.txt
+https://github.com/elypter/umatrix_rule_converter
+https://raw.githubusercontent.com/david1davidson/uMatrixMasterList/master/ruleset.txt
+https://raw.githubusercontent.com/stephengeorgewest/my-umatrix-rules/master/my-umatrix-rules.txt
+https://raw.githubusercontent.com/tranhoangcore/uMatrix-Rules/master/my-umatrix-rules.txt
+https://pve.proxmox.com/pve-docs/pve-admin-guide.pdf
+https://bitbucket.org/ethanr/dns-blacklists/src/master/
+https://github.com/techgaun/github-dorks/blob/master/github-dorks.txt
+https://raw.githubusercontent.com/1N3/Sn1per/master/osint.sh
+https://github.com/b3rito/yotter/blob/master/yotter.sh
+https://github.com/b3rito/yodo/blob/master/yodo.sh
+https://github.com/ShotokanZH/Pa-th-zuzu
+https://raw.githubusercontent.com/ShotokanZH/TorBAN/master/torban.sh
+https://raw.githubusercontent.com/nccgroup/holepuncher/master/holepuncher.sh
+https://github.com/nccgroup?utf8=%E2%9C%93&q=&type=&language=
+https://github.com/nccgroup/vlan-hopping---frogger/blob/master/frogger.sh
+https://github.com/nccgroup/port-scan-automation/blob/master/lazymap.sh
