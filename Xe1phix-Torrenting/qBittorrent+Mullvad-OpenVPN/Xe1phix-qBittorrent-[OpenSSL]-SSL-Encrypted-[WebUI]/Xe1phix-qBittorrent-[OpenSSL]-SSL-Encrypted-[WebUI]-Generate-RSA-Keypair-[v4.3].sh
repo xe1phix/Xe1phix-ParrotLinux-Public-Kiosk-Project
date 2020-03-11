2c889bbe38506fe156c12697c60e83b49d3c680b
Xe1phix <xe1phix@protonmail.ch>
2020-03-11 01:33:37 +0000

    Update Xe1phix-qBittorrent-_OpenSSL_-SSL-Encrypted-_WebUI__-Generate-RSA-Keypair-_v4.3_.sh

--- Xe1phix-Torrenting/qBittorrent+Mullvad-OpenVPN/Xe1phix-qBittorrent-[OpenSSL]-SSL-Encrypted-[WebUI]/Xe1phix-qBittorrent-[OpenSSL]-SSL-Encrypted-[WebUI]-Generate-RSA-Keypair-[v4.3].sh
+++ Xe1phix-Torrenting/qBittorrent+Mullvad-OpenVPN/Xe1phix-qBittorrent-[OpenSSL]-SSL-Encrypted-[WebUI]/Xe1phix-qBittorrent-[OpenSSL]-SSL-Encrypted-[WebUI]-Generate-RSA-Keypair-[v4.3].sh
@@ -5,4 +5,4 @@
 ## -------------------------------------------------------------------- ##
 ##   [?] https://letsencrypt.org/docs/certificates-for-localhost/
 ## -------------------------------------------------------------------- ##
-/usr/bin/openssl req -x509 -out Xe1phix-ParrotSec-Kiosk-LocalHost.crt -keyout Xe1phix-ParrotSec-Kiosk-LocalHost.key -newkey rsa:4096 -nodes -sha512 -subj '/CN=localhost' -extensions EXT -config <( printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+openssl req -x509 -out Xe1phix-ParrotSec-Kiosk-LocalHost.crt -keyout Xe1phix-ParrotSec-Kiosk-LocalHost.key -newkey rsa:4096 -nodes -sha512 -subj '/CN=localhost' -extensions EXT -config <( printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
