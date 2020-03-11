90a0bf8d94d86dbd011b6b0a9281a9a94a0e8eda
Xe1phix <xe1phix@protonmail.ch>
2020-03-11 01:28:15 +0000

    Upload New File

---
+++ Xe1phix-Torrenting/qBittorrent+Mullvad-OpenVPN/Xe1phix-qBittorrent-[OpenSSL]-SSL-Encrypted-[WebUI]/Xe1phix-qBittorrent-_OpenSSL_-SSL-Encrypted-_WebUI__-Generate-RSA-Keypair-_v4.2_.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+openssl req -x509 -out Xe1phix-ParrotSec-Kiosk-LocalHost.crt -keyout Xe1phix-ParrotSec-Kiosk-LocalHost.key -newkey rsa:4096 -nodes -sha512 -subj '/CN=localhost' -extensions EXT -config <( printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
