1a071c7ec9cb1692e2d956c732bb8cf7c97155f0
Xe1phix <xe1phix@protonmail.ch>
2021-01-17 22:43:14 +0000

    Update Xe1phix-[Git]/Xe1phix-ParrotSec-Kiosk-[Git-Clone]-Backup/Xe1phix-CipherPunk-[Git-Clone]-Beta/Xe1phix-[Git-Clone]-[.v2.4.5.]-[Appended]-{(.v8.4.5.})-[01-12-20].sh, Xe1phix-[Git]/Community-[Git-Clone]/Github-Clone-URLs-vsA766A4.txt, Xe1phix-[Git]/Xe1phix-Git-Clone-Bulk-[Awesome-Lists]-Extracted-URLs/Xe1phix-Git-Clone-Bulk-Hand-Selected-[Awesome-Lists]-{(Awesome-Project-Repos)}-Automated-Scraper-[.v5.2.8.]-(12-12-20).sh, Xe1phix-[Git]/Community-[Git-Clone]/roycewilliams-github-starred.md files

---
+++ Xe1phix-[Git]/Xe1phix-Git-Clone-Bulk-[Awesome-Lists]-Extracted-URLs/Xe1phix-Git-Clone-Bulk-Hand-Selected-[Awesome-Lists]-{(Awesome-Project-Repos)}-Automated-Scraper-[.v5.2.8.]-(12-12-20).sh
@@ -0,0 +1,348 @@
+
+
+
+
+https://github.com/InQuest/awesome-yara
+
+
+
+
+git clone https://github.com/zingchart/awesome-charting
+git clone https://github.com/zoidbergwill/awesome-ebpf
+git clone https://github.com/zudochkin/awesome-newsletters
+git clone https://github.com/zachflower/awesome-open-source-supporters
+git clone https://github.com/xen0l/awesome-dtrace
+git clone https://github.com/writing-resources/awesome-scientific-writing
+https://github.com/shiv4nsh/awesome-computer-science-resources
+
+git clone https://github.com/weblancaster/awesome-IoT-hybrid
+git clone https://github.com/webpack-contrib/awesome-webpack
+git clone https://github.com/webpro/awesome-dotfiles
+git clone https://github.com/willianjusten/awesome-audio-visualization
+git clone https://github.com/willianjusten/awesome-svg
+git clone https://github.com/woop/awesome-quantified-self
+git clone https://github.com/uraimo/awesome-software-patreons
+git clone https://github.com/veggiemonk/awesome-docker
+git clone https://github.com/victorlaerte/awesome-it-quotes
+git clone https://github.com/victorshinya/awesome-ibmcloud
+git clone https://github.com/vinkla/shareable-links
+git clone https://github.com/vinta/awesome-python
+git clone https://github.com/wasabeef/awesome-android-ui
+git clone https://github.com/watson/awesome-computer-history
+git clone https://github.com/unixorn/awesome-zsh-plugins
+git clone https://github.com/umutphp/awesome-cli
+git clone https://github.com/ucg8j/awesome-dash
+git clone https://github.com/shime/creative-commons-media
+git clone https://github.com/siboehm/awesome-learn-datascience
+git clone https://github.com/simskij/awesome-software-architecture
+git clone https://github.com/sindresorhus/awesome
+git clone https://github.com/sindresorhus/awesome-scifi
+git clone https://github.com/sobolevn/awesome-cryptography
+git clone https://github.com/stevemao/awesome-git-addons
+git clone https://github.com/stevesong/awesome-connectivity-info
+git clone https://github.com/stingalleman/awesome-audiovisual
+git clone https://github.com/theimpossibleastronaut/awesome-linguistics
+git clone https://github.com/thibmaek/awesome-raspberry-pi
+git clone https://github.com/tiimgreen/github-cheat-sheet
+git clone https://github.com/tleb/awesome-mastodon
+git clone https://github.com/tokenmill/awesome-nlg
+git clone https://github.com/tramcar/awesome-job-boards
+git clone https://github.com/transitive-bullshit/awesome-ffmpeg
+git clone https://github.com/ttumiel/Awesome-Ethereum
+git clone https://github.com/tvvocold/FOSS-for-Dev
+git clone https://github.com/onmyway133/awesome-voip
+git clone https://github.com/sbilly/awesome-security
+git clone https://github.com/sduff/awesome-splunk
+git clone https://github.com/rshipp/awesome-malware-analysis
+git clone https://github.com/guillaume-chevalier/awesome-deep-learning-resources
+git clone https://github.com/gruhn/awesome-naming
+git clone https://github.com/git-tips/tips
+git clone https://github.com/eselkin/awesome-computational-neuroscience
+git clone https://github.com/exAspArk/awesome-chatops
+git clone https://github.com/expede/awesome-relay
+git clone https://github.com/faroit/awesome-python-scientific-audio
+git clone https://github.com/fasouto/awesome-dataviz
+git clone https://github.com/fcambus/nginx-resources
+git clone https://github.com/feross/awesome-mad-science
+git clone https://github.com/filipelinhares/awesome-slack
+git clone https://github.com/fkie-cad/awesome-embedded-and-iot-security
+git clone https://github.com/francoism90/awesome-kde
+git clone https://github.com/fregante/Awesome-WebExtensions
+git clone https://github.com/fuse-compound/awesome-fuse
+git clone https://github.com/gaerae/awesome-algorithms-education
+git clone https://github.com/emptymalei/awesome-research
+git clone https://github.com/ebu/awesome-broadcasting
+git clone https://github.com/dok/awesome-text-editing
+git clone https://github.com/donnemartin/awesome-aws
+git clone https://github.com/dreamingechoes/awesome-mental-health
+git clone https://github.com/drewrwilson/toolsforactivism
+git clone https://github.com/drmonkeyninja/awesome-textpattern
+git clone https://github.com/dylanrees/citizen-science
+git clone https://github.com/dypsilon/frontend-dev-bookmarks
+git clone https://github.com/diegocard/awesome-html5
+git clone https://github.com/desireevl/awesome-quantum-computing
+git clone https://github.com/dend/awesome-product-management
+git clone https://github.com/daviddias/awesome-hacking-locations
+git clone https://github.com/davisonio/awesome-gif
+git clone https://github.com/davisonio/awesome-irc
+git clone https://github.com/cpuu/awesome-fuzzing
+git clone https://github.com/danielecook/Awesome-Bioinformatics
+git clone https://github.com/danvoyce/awesome-creative-tech-events
+git clone https://github.com/compscilauren/awesome-git-hooks
+git clone https://github.com/cyberglot/awesome-answers
+git clone https://github.com/cenoura/awesome-ads
+git clone https://github.com/ciconia/awesome-music
+git clone https://github.com/cjbarber/ToolsOfTheTrade
+git clone https://github.com/caufieldjh/awesome-bioie
+git clone https://github.com/awesome-selfhosted/awesome-selfhosted
+git clone https://github.com/ayr-ton/awesome-geek-podcasts
+git clone https://github.com/briatte/awesome-network-analysis
+git clone https://github.com/caesar0301/awesome-pcaptools
+git clone https://github.com/carpedm20/awesome-hacking
+git clone https://github.com/PandaFoss/Awesome-Arch
+git clone https://github.com/TalAter/awesome-book-authoring
+git clone https://github.com/TalAter/awesome-service-workers
+git clone https://github.com/TaptuIT/awesome-devsecops
+git clone https://github.com/Wisdom/Awesome-Unicode
+git clone https://github.com/Zheaoli/awesome-coins
+git clone https://github.com/academic/awesome-datascience
+git clone https://github.com/agarrharr/awesome-cli-apps
+git clone https://github.com/agarrharr/awesome-static-website-services
+git clone https://github.com/agucova/awesome-esp
+git clone https://github.com/alebcay/awesome-shell
+git clone https://github.com/aleksandar-todorovic/awesome-c
+
+https://github.com/myugan/awesome-linux-customization
+git clone https://github.com/aleksandar-todorovic/awesome-linux
+https://github.com/LinuxUsersGroup-NU/Awesome-linux
+
+
+git clone https://github.com/alexk111/awesome-bitcoin-payment-processors
+
+git clone https://github.com/analysis-tools-dev/static-analysis
+git clone https://github.com/analyticalmonk/awesome-neuroscience
+git clone https://github.com/apsdehal/awesome-ctf
+git clone https://github.com/arslanbilal/git-cheat-sheet
+git clone https://github.com/ashishb/android-security-awesome
+
+
+git clone https://github.com/LucasBassetti/awesome-less
+git clone https://github.com/MartinMiles/Awesome-Sitecore
+
+
+git clone https://github.com/MaximAbramchuck/awesome-interview-questions
+https://github.com/Nickersoft/awesome-careers
+https://github.com/kitze/awesome-conference-practices
+https://github.com/InterviewMap/CS-Interview-Knowledge-Map
+
+
+
+
+git clone https://github.com/MunGell/awesome-for-beginners
+git clone https://github.com/NARKOZ/guides
+git clone https://github.com/NoahBuscher/Inspire
+
+
+
+git clone https://github.com/JStumpp/awesome-android
+git clone https://github.com/JanVanRyswyck/awesome-talks
+git clone https://github.com/JesseTG/awesome-qt
+git clone https://github.com/Kazhnuz/awesome-gnome
+git clone https://github.com/HQarroum/awesome-iot
+git clone https://github.com/Friz-zy/awesome-linux-containers
+git clone https://github.com/EbookFoundation/free-programming-books
+git clone https://github.com/DataDaoDe/awesome-foss-apps
+
+
+https://github.com/yeyintminthuhtut/Awesome-Red-Teaming
+https://github.com/0x4D31/awesome-oscp
+
+git clone https://github.com/enaqx/awesome-pentest
+
+git clone https://github.com/nixawk/pentest-wiki
+git clone https://github.com/nixawk/pentest-wiki.wiki.git
+
+git clone https://github.com/gradiuscypher/infosec_getting_started
+git clone https://github.com/DoGByTe-ZN/infosec-resources4all
+https://github.com/vitalysim/Awesome-Hacking-Resources
+
+git clone https://github.com/ahmetpergamum/infosecTraining
+git clone https://github.com/joe-shenouda/awesome-cyber-skills
+https://github.com/kdn251/interviews
+https://github.com/Twipped/InterviewThis
+
+https://github.com/samsquire/ideas
+
+
+
+git clone https://github.com/jakejarvis/awesome-shodan-queries
+git clone https://github.com/remiflavien1/awesome-anti-forensic
+
+
+https://github.com/cometkim/awesome-list
+git clone https://github.com/jnv/lists
+https://github.com/bayandin/awesome-awesomeness
+
+
+
+
+
+
+
+git clone https://github.com/hackerkid/Mind-Expanding-Books
+git clone https://github.com/hackerkid/bots
+git clone https://github.com/harpribot/awesome-information-retrieval
+git clone https://github.com/hsiaoyi0504/awesome-cheminformatics
+git clone https://github.com/hubtee/awesome-opensource-documents
+git clone https://github.com/humanetech-community/awesome-humane-tech
+git clone https://github.com/iJackUA/awesome-vagrant
+git clone https://github.com/ibaaj/awesome-OpenSourcePhotography
+git clone https://github.com/igorbarinov/awesome-bitcoin
+git clone https://github.com/igorbarinov/awesome-data-engineering
+git clone https://github.com/iipc/awesome-web-archiving
+git clone https://github.com/ipfs/awesome-ipfs
+git clone https://github.com/irazasyed/awesome-cloudflare
+git clone https://github.com/jaredthecoder/awesome-vehicle-security
+git clone https://github.com/jbhuang0604/awesome-computer-vision
+git clone https://github.com/johnjago/awesome-ad-free
+git clone https://github.com/johnjago/awesome-free-software
+git clone https://github.com/jonathandion/awesome-emails
+git clone https://github.com/jondot/awesome-devenv
+git clone https://github.com/jonleibowitz/awesome-digitalocean
+git clone https://github.com/jtoy/awesome-tensorflow
+git clone https://github.com/jyguyomarch/awesome-productivity
+git clone https://github.com/k4m4/terminals-are-sexy
+git clone https://github.com/kakoni/awesome-healthcare
+git clone https://github.com/karlhorky/awesome-speakers
+git clone https://github.com/karlhorky/learn-to-program
+git clone https://github.com/kilimchoi/engineering-blogs
+https://github.com/rushter/data-science-blogs
+git clone https://github.com/kitspace/awesome-electronics
+git clone https://github.com/krzjoa/awesome-python-data-science
+git clone https://github.com/kyleterry/awesome-radio
+git clone https://github.com/liuchong/awesome-roadmaps
+git clone https://github.com/lucasviola/awesome-functional-programming
+git clone https://github.com/lucasviola/awesome-tech-videos
+git clone https://github.com/lukasz-madon/awesome-remote-job
+git clone https://github.com/lurumdare/awesome-asm
+git clone https://github.com/lvwzhen/tools
+git clone https://github.com/machinomy/awesome-non-financial-blockchain
+git clone https://github.com/maehr/awesome-digital-history
+git clone https://github.com/manuzhang/awesome-streaming
+git clone https://github.com/marcobiedermann/search-engine-optimization
+git clone https://github.com/AchoArnold/discount-for-student-dev
+git clone https://github.com/BubuAnabelas/awesome-markdown
+git clone https://github.com/ChromeDevTools/awesome-chrome-devtools
+git clone https://github.com/matchai/awesome-pinned-gists
+git clone https://github.com/matiassingers/awesome-readme
+git clone https://github.com/matiassingers/awesome-slack
+git clone https://github.com/matteofigus/awesome-speaking
+https://github.com/dwmkerr/hacker-laws
+git clone https://github.com/meirwah/awesome-incident-response
+git clone https://github.com/meitar/awesome-cybersecurity-blueteam
+git clone https://github.com/meitar/awesome-lockpicking
+git clone https://github.com/melvin0008/awesome-projects-boilerplates
+git clone https://github.com/mfornos/awesome-microservices
+git clone https://github.com/mislavcimpersak/awesome-dev-fun
+git clone https://github.com/mmcgrana/services-engineering
+git clone https://github.com/moul/awesome-ssh
+git clone https://github.com/myshov/awesome-mobile-web-development
+git clone https://github.com/n1trux/awesome-sysadmin
+git clone https://github.com/nayafia/awesome-maintainers
+git clone https://github.com/neutraltone/awesome-stock-resources
+git clone https://github.com/nglgzz/awesome-clean-tech
+git clone https://github.com/nicolesaidy/awesome-web-design
+git clone https://github.com/notlmn/awesome-icons
+git clone https://github.com/notthetup/awesome-webaudio
+git clone https://github.com/nschloe/awesome-scientific-computing
+git clone https://github.com/onurakpolat/awesome-analytics
+git clone https://github.com/onurakpolat/awesome-bigdata
+git clone https://github.com/opencompany/awesome-open-company
+git clone https://github.com/ossu/computer-science
+git clone https://github.com/pFarb/awesome-crypto-papers
+git clone https://github.com/papers-we-love/papers-we-love
+git clone https://github.com/paragonie/awesome-appsec
+git clone https://github.com/paralax/awesome-honeypots
+git clone https://github.com/phillipadsmith/awesome-github
+
+git clone https://github.com/pmuens/awesome-serverless
+https://github.com/puresec/awesome-serverless-security/
+
+git clone https://github.com/prakhar1989/awesome-courses
+git clone https://github.com/qazbnm456/awesome-web-security
+https://github.com/s0md3v/AwesomeXSS
+git clone https://github.com/ripienaar/free-for-dev
+git clone https://github.com/xasos/awesome-decentralized-papers
+git clone https://github.com/bekatom/awesome-ethereum
+git clone https://github.com/steve-vincent/awesome-decentralized
+git clone https://github.com/retrohacker/awesome-p2p
+git clone https://github.com/danoctavian/awesome-anti-censorship
+git clone https://github.com/fabianhjr/awesome-radical-decentralization
+git clone https://github.com/saaperezru/awesome-decentralized-web
+git clone https://github.com/ong/awesome-decentralized-finance
+git clone https://github.com/gdamdam/awesome-decentralized-web
+git clone https://github.com/croqaz/awesome-decentralized
+git clone https://github.com/holybao/awesome-decentralized-storage
+git clone https://github.com/Mikerah/awesome-privacy-on-blockchains
+git clone https://github.com/noxstream/decentralised-web
+
+git clone https://github.com/thibmaek/awesome-raspberry-pi
+git clone https://github.com/DocNow/awesome-archive-tools
+git clone https://github.com/IVMachiavelli/OSINT_Team_Links
+
+
+
+
+
+git clone https://github.com/BlockchainDevs/CryptocurrencyAwesome
+git clone https://github.com/void4/awesome-ethereum
+git clone https://github.com/vinsgo/awesome-ethereum
+git clone https://github.com/bekatom/awesome-ethereum
+git clone https://github.com/kgryte/awesome-peer-to-peer
+git clone https://github.com/cserb/awesome-decentralization
+git clone https://github.com/igorbarinov/awesome-bitcoin
+git clone https://github.com/JoshuaEstes/awesome-bitcoin
+git clone https://github.com/btcbrdev/awesome-btcdev
+git clone 
+git clone 
+git clone 
+git clone 
+git clone 
+git clone 
+git clone 
+git clone 
+git clone https://github.com/xMG-ReduX/Awesome-Onion-Links
+git clone https://github.com/ajvb/awesome-tor
+git clone https://github.com/awesomeguides/awesome-tor
+git clone 
+git clone 
+git clone 
+git clone 
+git clone 
+git clone 
+git clone 
+git clone 
+git clone https://github.com/remiflavien1/awesome-darknet
+git clone https://github.com/mikalv/awesome-i2p
+git clone https://github.com/KevinScruff/awesome-zites
+git clone https://github.com/hexlab/awesome-peer-to-peer
+git clone 
+git clone https://github.com/status-im/awesome-secure-messaging
+git clone 
+git clone https://github.com/Igglybuff/awesome-piracy
+git clone https://github.com/p0lygun/awesome-pirate
+git clone https://github.com/awesomeguides/awesome-torrent
+git clone https://github.com/HDVinnie/awesome-torrent-resources
+git clone https://github.com/jamesacampbell/awesome-pastes
+git clone 
+git clone https://github.com/2gatherproject/decentralized-social-apps-guide
+git clone https://github.com/unexpectedBy/awesome-pentest-tools
+git clone 
+git clone 
+git clone https://github.com/humanetech-community/awesome-humane-tech
+git clone https://github.com/lyfeyaj/awesome-resources
+git clone https://github.com/epoyraz/Awesome-Youtube-Channels
+git clone 
+git clone 
+git clone 
+
