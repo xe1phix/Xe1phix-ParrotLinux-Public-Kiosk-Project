f39b4aa1735cc0be8f80c81dc39a88a85ac5935d
Xe1phix <xe1phix@protonmail.ch>
2021-05-04 02:07:47 +0000

    Update Xe1phix-[.Configs]/Xe1phix-[Folder-Administration]/Xe1phix-[Firejail]-Folder-Administration/Xe1phix-[Firejail]-Directories-[v9.4.8].txt, Xe1phix-[.Configs]/Xe1phix-[Folder-Administration]/Xe1phix-[Podcast]-Folder-Administration/Xe1phix-Folder-Administration-[Creation]-Podcast-Folders-[v4.8.2].sh, Xe1phix-[.Configs]/Xe1phix-[Folder-Administration]/Xe1phix-[2TB-Desktop]-Folder-Administration/Xe1phix-[Textbooks]-Folder-Administration/TextbooksList.txt, Xe1phix-[.Configs]/Xe1phix-[Folder-Administration]/Xe1phix-[4Chan]-Folder-Administration/Xe1phix-[4Chan-Webms]-Folder-Administration/Xe1phix-4Chan-[WGet-GIF-Template]-{(i.4cdn.org+is2.4Chan.org)}-[.v15.8.2.]-{(04-30-21)}.sh.gpg, Xe1phix-[.Configs]/Xe1phix-[Folder-Administration]/Xe1phix-[Audio]-Folder-Administration/Xe1phix-Folder-Administration-[Creation]-Audio-Folders-[v4.5.2].sh, Xe1phix-[.Configs]/Xe1phix-[Folder-Administration]/Xe1phix-[Torrenting]-Folder-Administration/Xe1phix-Torrenting-[Torrent-Directories]-Folder-Administration/Xe1phix-[Torrents]-Folder-Administration-[Creation]-Torrents-Other-Folders-[v4.5.2].sh.gpg files Deleted Xe1phix-.Configs/Xe1phix-[Folder-Administration]/Xe1phix-[2TB-Desktop]-Folder-Administration/Xe1phix-ParrotSec-Kiosk-Project-[Production]-Folder-Administration/.gitkeep, Xe1phix-.Configs/Xe1phix-[Folder-Administration]/Xe1phix-[2TB-Desktop]-Folder-Administration/Xe1phix-[Textbooks]-Folder-Administration/.gitkeep, Xe1phix-.Configs/Xe1phix-[Folder-Administration]/Xe1phix-[2TB-Desktop]-Folder-Administration/.gitkeep, Xe1phix-.Configs/Xe1phix-[Folder-Administration]/Xe1phix-[4Chan]-Folder-Administration/Xe1phix-[4Chan-Webms]-Folder-Administration/.gitkeep, Xe1phix-.Configs/Xe1phix-[Folder-Administration]/Xe1phix-[4Chan]-Folder-Administration/.gitkeep, Xe1phix-.Configs/Xe1phix-[Folder-Administration]/Xe1phix-[Audio]-Folder-Administration/.gitkeep, Xe1phix-.Configs/Xe1phix-[Folder-Administration]/Xe1phix-[Firejail]-Folder-Administration/.gitkeep, Xe1phix-.Configs/Xe1phix-[Folder-Administration]/Xe1phix-[Gitlab]-Folder-Administration/Xe1phix-[Firefox-Hardening]-Folder-Administration/.gitkeep, Xe1phix-.Configs/Xe1phix-[Folder-Administration]/Xe1phix-[Gitlab]-Folder-Administration/Xe1phix-[ParrotSec-Kiosk-Project]-Folder-Administration/.gitkeep, Xe1phix-.Configs/Xe1phix-[Folder-Administration]/Xe1phix-[Gitlab]-Folder-Administration/.gitkeep, Xe1phix-.Configs/Xe1phix-[Folder-Administration]/Xe1phix-[Podcast]-Folder-Administration/.gitkeep, Xe1phix-.Configs/Xe1phix-[Folder-Administration]/Xe1phix-[Torrenting]-Folder-Administration/Xe1phix-Torrenting-[Torrent-Directories]-Folder-Administration/.gitkeep, Xe1phix-.Configs/Xe1phix-[Folder-Administration]/Xe1phix-[Torrenting]-Folder-Administration/Xe1phix-Torrenting-[Torrent-Files]-Folder-Administration/.gitkeep, Xe1phix-.Configs/Xe1phix-[Folder-Administration]/Xe1phix-[Torrenting]-Folder-Administration/Xe1phix-Torrenting-[Torrent-Screens]-Folder-Administration/.gitkeep, Xe1phix-.Configs/Xe1phix-[Folder-Administration]/Xe1phix-[Torrenting]-Folder-Administration/.gitkeep, Xe1phix-.Configs/Xe1phix-[Folder-Administration]/.gitkeep, Xe1phix-.Configs/Xe1phix-[GTK-Bookmarks]/Caja-Bookmarks/.gitkeep, Xe1phix-.Configs/Xe1phix-[GTK-Bookmarks]/Caja-Bookmarks/Xe1phix-GTK-Bookmarks-v10.7.txt, Xe1phix-.Configs/Xe1phix-[GTK-Bookmarks]/Caja-Bookmarks/Xe1phix-GTK-Bookmarks-v11.2.txt, Xe1phix-.Configs/Xe1phix-[GTK-Bookmarks]/Caja-Bookmarks/Xe1phix-GTK-Bookmarks-v12.8.txt, Xe1phix-.Configs/Xe1phix-[GTK-Bookmarks]/Caja-Bookmarks/Xe1phix-GTK-Bookmarks-v9.5.txt, Xe1phix-.Configs/Xe1phix-[GTK-Bookmarks]/ParrotSec-Kiosk-Persistent-Bookmarks/.gitkeep, Xe1phix-.Configs/Xe1phix-[GTK-Bookmarks]/ParrotSec-Kiosk-Persistent-Bookmarks/ParrotSec-Kiosk-Persistent-Bookmarks-v4.7.txt, Xe1phix-.Configs/Xe1phix-[GTK-Bookmarks]/.gitkeep, Xe1phix-.Configs/.gitkeep files

---
+++ Xe1phix-[.Configs]/Xe1phix-[Folder-Administration]/Xe1phix-[Audio]-Folder-Administration/Xe1phix-Folder-Administration-[Creation]-Audio-Folders-[v4.5.2].sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+## -------------------------------------------------------------------------------------------------------------- ##
+##   [+] Xe1phix-Folder-Administration-[Creation]-Audio-Folders-[v*.*.*].sh
+## -------------------------------------------------------------------------------------------------------------- ##
+mkdir --verbose --parents ~/Downloads/Audio/10-Years/
+mkdir --verbose --parents ~/Downloads/Audio/A-Perfect-Circle/
+mkdir --verbose --parents ~/Downloads/Audio/Aphex-Twin/
+mkdir --verbose --parents ~/Downloads/Audio/Arch-Enemy/
+mkdir --verbose --parents ~/Downloads/Audio/Atreyu/
+mkdir --verbose --parents ~/Downloads/Audio/Black-Sabbath/
+mkdir --verbose --parents ~/Downloads/Audio/Buckethead/
+mkdir --verbose --parents ~/Downloads/Audio/Children-O̲f̲-Bodom/
+mkdir --verbose --parents ~/Downloads/Audio/CollectiveSoul/
+mkdir --verbose --parents ~/Downloads/Audio/Daron-Malakian-and-Scars-on-Broadway---Dictator/
+mkdir --verbose --parents ~/Downloads/Audio/Days-of-The-New/
+mkdir --verbose --parents ~/Downloads/Audio/Death/
+mkdir --verbose --parents ~/Downloads/Audio/Defcon/
+mkdir --verbose --parents ~/Downloads/Audio/Destruction/
+mkdir --verbose --parents ~/Downloads/Audio/Electronica/
+mkdir --verbose --parents ~/Downloads/Audio/Elena-Verrier/
+mkdir --verbose --parents ~/Downloads/Audio/Exodus/
+mkdir --verbose --parents ~/Downloads/Audio/Fear-Inoculum/
+mkdir --verbose --parents ~/Downloads/Audio/Hatebreed/
+mkdir --verbose --parents ~/Downloads/Audio/Infected-Mushroom/
+mkdir --verbose --parents ~/Downloads/Audio/Judas-Priest/
+mkdir --verbose --parents ~/Downloads/Audio/King-Diamond/
+mkdir --verbose --parents ~/Downloads/Audio/Korn/
+mkdir --verbose --parents ~/Downloads/Audio/Kreator/
+mkdir --verbose --parents ~/Downloads/Audio/Lamb-of-Dod/
+mkdir --verbose --parents ~/Downloads/Audio/MetaPhysics/
+mkdir --verbose --parents ~/Downloads/Audio/Motorhead/
+mkdir --verbose --parents ~/Downloads/Audio/NerdCore/
+mkdir --verbose --parents ~/Downloads/Audio/NOFX/
+mkdir --verbose --parents ~/Downloads/Audio/Pantera/
+mkdir --verbose --parents ~/Downloads/Audio/Psychology/
+mkdir --verbose --parents ~/Downloads/Audio/Puscifer
+mkdir --verbose --parents ~/Downloads/Audio/Savatage/
+mkdir --verbose --parents ~/Downloads/Audio/Scars-On-Broadway
+mkdir --verbose --parents ~/Downloads/Audio/Slipknot/
+mkdir --verbose --parents ~/Downloads/Audio/Sodom/
+mkdir --verbose --parents ~/Downloads/Audio/Stormtroopers-Of-Death-(S.O.D.)/
+mkdir --verbose --parents ~/Downloads/Audio/Sum41/
+mkdir --verbose --parents ~/Downloads/Audio/[Symphony-of-Science]-Astro+Quantum-Physics-[Chillstep]/
+mkdir --verbose --parents ~/Downloads/Audio/System-of-A-Down/
+mkdir --verbose --parents ~/Downloads/Audio/Temple-of-The-Dog/
+mkdir --verbose --parents ~/Downloads/Audio/Testament/
+mkdir --verbose --parents ~/Downloads/Audio/That-One-Guy/
+mkdir --verbose --parents ~/Downloads/Audio/The-Exploited/
+mkdir --verbose --parents ~/Downloads/Audio/The-Misfits/
+mkdir --verbose --parents ~/Downloads/Audio/White-Zombie/
+##  mkdir --verbose --parents ~/Downloads/Audio/
+##  mkdir --verbose --parents ~/Downloads/Audio/
+##  mkdir --verbose --parents ~/Downloads/Audio/
+##  mkdir --verbose --parents ~/Downloads/Audio/
+##  mkdir --verbose --parents ~/Downloads/Audio/
+##  mkdir --verbose --parents ~/Downloads/Audio/
+##  mkdir --verbose --parents ~/Downloads/Audio/
+##  mkdir --verbose --parents ~/Downloads/Audio/
+##  mkdir --verbose --parents ~/Downloads/Audio/
+##  mkdir --verbose --parents ~/Downloads/Audio/
---
+++ Xe1phix-[.Configs]/Xe1phix-[Folder-Administration]/Xe1phix-[Podcast]-Folder-Administration/Xe1phix-Folder-Administration-[Creation]-Podcast-Folders-[v4.8.2].sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+## -------------------------------------------------------------------------------------------------------------- ##
+##   [+] Xe1phix-Folder-Administration-[Creation]-Podcast-Folders-[v*.*.*].sh
+## -------------------------------------------------------------------------------------------------------------- ##
+mkdir --verbose --parents ~/Downloads/Podcasts/Legion-of-Skanks
+mkdir --verbose --parents ~/Downloads/Podcasts/The-Bonfire
+mkdir --verbose --parents ~/Downloads/Podcasts/Ari-Shaffirs-Skeptic-Tank
+mkdir --verbose --parents ~/Downloads/Podcasts/The-Church-of-Whats-Happening-Now
+mkdir --verbose --parents ~/Downloads/Podcasts/Doug-Stanhope-Podcast
+mkdir --verbose --parents ~/Downloads/Podcasts/Duncan-Trussell-Family-Hour
+mkdir --verbose --parents ~/Downloads/Podcasts/Robert-Kelly-You-Know-What-Dude
+mkdir --verbose --parents ~/Downloads/Podcasts/Bone-Zone
+mkdir --verbose --parents ~/Downloads/Podcasts/JRE
+mkdir --verbose --parents ~/Downloads/Podcasts/Bill-Burr-Monday-Morning
+mkdir --verbose --parents ~/Downloads/Podcasts/Your-Moms-House
+mkdir --verbose --parents ~/Downloads/Podcasts/Punch-Drunk-Sports
+mkdir --verbose --parents ~/Downloads/Podcasts/Beers-with-Talos
+mkdir --verbose --parents ~/Downloads/Podcasts/Exotic-Liability
+mkdir --verbose --parents ~/Downloads/Podcasts/TrustedSec
+mkdir --verbose --parents ~/Downloads/Podcasts/Pauls-Security-Weekly
+mkdir --verbose --parents ~/Downloads/Podcasts/Darknet-Diaries
+mkdir --verbose --parents ~/Downloads/Podcasts/Ask-Noah
+mkdir --verbose --parents ~/Downloads/Podcasts/Coder-Radio
+mkdir --verbose --parents ~/Downloads/Podcasts/LINUX-Unplugged
+mkdir --verbose --parents ~/Downloads/Podcasts/Self-Hosted
+mkdir --verbose --parents ~/Downloads/Podcasts/Linux-Action-News
+mkdir --verbose --parents ~/Downloads/Podcasts/BSD-Now
+mkdir --verbose --parents ~/Downloads/Podcasts/User-Error
+mkdir --verbose --parents ~/Downloads/Podcasts/TechSNAP
+mkdir --verbose --parents ~/Downloads/Podcasts/Jimmy-Dore-Show
+mkdir --verbose --parents ~/Downloads/Podcasts/Tin-Foil-Hat-With-Sam-Tripoli
+mkdir --verbose --parents ~/Downloads/Podcasts/InfoWars
+mkdir --verbose --parents ~/Downloads/Podcasts/Sam-Harris-WakingUp
+mkdir --verbose --parents ~/Downloads/Podcasts/Tangentially-Speaking
+mkdir --verbose --parents ~/Downloads/Podcasts/Ram-Dass---Be-Here-Now
+mkdir --verbose --parents ~/Downloads/Podcasts/Jack-Kornfield-Heart-Wisdom
+mkdir --verbose --parents ~/Downloads/Podcasts/Bertrand-Russell
+mkdir --verbose --parents ~/Downloads/Podcasts/René-Descartes
+mkdir --verbose --parents ~/Downloads/Podcasts/William-James
+mkdir --verbose --parents ~/Downloads/Podcasts/Red-Hat-Command-Line-Heroes
+mkdir --verbose --parents ~/Downloads/Podcasts/Bruce-Schneier
+mkdir --verbose --parents ~/Downloads/Podcasts/Jim-Florentine
+mkdir --verbose --parents ~/Downloads/Podcasts/Real-Ass-Podcast
+mkdir --verbose --parents ~/Downloads/Podcasts/Part-Of-The-Problem
+mkdir --verbose --parents ~/Downloads/Podcasts/Cant-Get-Right-with-Kurt-Metzger
+mkdir --verbose --parents ~/Downloads/Podcasts/In-Godfrey-We-Trust
+mkdir --verbose --parents ~/Downloads/Podcasts/WTF-With-Marc-Maron
+mkdir --verbose --parents ~/Downloads/Podcasts/Fitzdog-Radio
+mkdir --verbose --parents ~/Downloads/Podcasts/Jim-Jefferies
+mkdir --verbose --parents ~/Downloads/Podcasts/CCC-Chaos-Radio
+mkdir --verbose --parents ~/Downloads/Podcasts/Hak5
+mkdir --verbose --parents ~/Downloads/Podcasts/Forensic-Lunch
+mkdir --verbose --parents ~/Downloads/Podcasts/StarTalk
+mkdir --verbose --parents ~/Downloads/Podcasts/Off-The-Hook
+mkdir --verbose --parents ~/Downloads/Podcasts/Off-The-Wall
+mkdir --verbose --parents ~/Downloads/Podcasts/MAPS-Podcast
+mkdir --verbose --parents ~/Downloads/Podcasts/Aubrey-Marcus-Warrior-Poet
+mkdir --verbose --parents ~/Downloads/Podcasts/Opie-And-Anthony
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
+##  mkdir --verbose --parents ~/Downloads/Podcasts/
