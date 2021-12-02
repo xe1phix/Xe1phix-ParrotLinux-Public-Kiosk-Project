d73217407ea9da4254db57ecc430741afe4574e8
Xe1phix <xe1phix@protonmail.ch>
2021-12-02 05:28:23 +0000

    Update Xe1phix-[Firejail]/Xe1phix-Firejail-[Linux-Capabilities]-(POSIX 1003.1e)/Grsecurity-WikiBook-[Linux-Capabilities]-(POSIX-1003.1e)-Names-and-Descriptions-Cheatsheet.pdf, Xe1phix-[Firejail]/Xe1phix-Firejail-[Linux-Capabilities]-(POSIX 1003.1e)/LinuxCaps-3.png, Xe1phix-[Firejail]/Xe1phix-Firejail-[Linux-Capabilities]-(POSIX 1003.1e)/LinuxCaps-5.png, Xe1phix-[Firejail]/Xe1phix-Firejail-[Linux-Capabilities]-(POSIX 1003.1e)/LinuxCapabilities-Table-1.png, Xe1phix-[Firejail]/Xe1phix-Firejail-[Linux-Capabilities]-(POSIX 1003.1e)/LinuxCaps-2.png, Xe1phix-[Firejail]/Xe1phix-Firejail-[Linux-Capabilities]-(POSIX 1003.1e)/LinuxCaps-6.png, Xe1phix-[Firejail]/Xe1phix-Firejail-[Linux-Capabilities]-(POSIX 1003.1e)/LinuxCaps6-2.png, Xe1phix-[Firejail]/Xe1phix-Firejail-[Linux-Capabilities]-(POSIX 1003.1e)/CapSysResource.png, Xe1phix-[Firejail]/Xe1phix-Firejail-[Linux-Capabilities]-(POSIX 1003.1e)/caps-4-CAP_SYS_ADMIN.png, Xe1phix-[Namespaces]/Namespace-Graph-2.jpg, Xe1phix-[Namespaces]/Namespaces-Graph.jpg, Xe1phix-[EDU]/Xe1phix-[.Nootropics-Research.]/Cognizin-[Citicoline]-Research/cognizin-citicoline-infographic-1.jpg, Xe1phix-[EDU]/Xe1phix-[.Nootropics-Research.]/Cognizin-[Citicoline]-Research/cognizin-citicoline-infographic-2.jpg, Xe1phix-[EDU]/Xe1phix-[.Nootropics-Research.]/Cognizin-[Citicoline]-Research/cognizin-infographic-3.jpg, Xe1phix-[EDU]/Xe1phix-[.Nootropics-Research.]/Cognizin-[Citicoline]-Research/27-kyowa-cognizin-citicoline.jpeg, Xe1phix-[EDU]/Xe1phix-[.Nootropics-Research.]/Cognizin-[Citicoline]-Research/cognizin-citicoline-infographic.jpeg, Xe1phix-[EDU]/Xe1phix-[.Nootropics-Research.]/Cognizin-[Citicoline]-Research/cognizin-citicoline-infographic.jpg, Xe1phix-[EDU]/Xe1phix-[.Nootropics-Research.]/Xe1phix-Nootropics-Research-[LionsMane]/BN2018-5802634.003.jpg, Xe1phix-[EDU]/Xe1phix-[.Nootropics-Research.]/Xe1phix-Nootropics-Research-[LionsMane]/LionsMane-Trials.png, Xe1phix-[EDU]/Xe1phix-[.Nootropics-Research.]/Xe1phix-Nootropics-Research-[LionsMane]/Mushrooms.png, Xe1phix-[EDU]/Xe1phix-[.Nootropics-Research.]/Xe1phix-Nootropics-Research-[LionsMane]/LionsMane-Effects.png, Xe1phix-[EDU]/Xe1phix-[.Nootropics-Research.]/Xe1phix-[Phenylpiracetam]/Phenylpiracetam-increases-operant-behavior.png, Xe1phix-[EDU]/Xe1phix-[.Nootropics-Research.]/Xe1phix-[Phenylpiracetam]/Phenylpiracetam-Operant-behavior.png, Xe1phix-[EDU]/Xe1phix-[.NeuroPharmacology]/618px-Cholinergic_synapse.svg.png, Xe1phix-[EDU]/Xe1phix-[.NeuroPharmacology]/ch6f8.jpg, Xe1phix-[EDU]/Xe1phix-[.NeuroPharmacology]/Neurotransmitter_System.jpeg, Xe1phix-[EDU]/Xe1phix-[.NeuroPharmacology]/SynapseSchematic_en.svg.png, Xe1phix-[EDU]/Xe1phix-[.NeuroPharmacology]/Neuropharmacology.sh, Xe1phix-[EDU]/Xe1phix-[.NeuroPharmacology]/NeuroQuantumCoherence.txt, Xe1phix-[EDU]/Xe1phix-[.NeuroPharmacology]/PerceptionsEffectsOnNeuronalSignaling.txt, Xe1phix-[EDU]/Xe1phix-[.NeuroPharmacology]/TAAR1_Amphetamine_Dopamine.png, Xe1phix-[EDU]/Xe1phix-[.NeuroPharmacology]/PsychoActive-Chemicals-Interactions-Chart.png, Xe1phix-[EDU]/Xe1phix-[.NeuroPharmacology]/Amino_Acids.svg.png, Xe1phix-[EDU]/Xe1phix-[.NeuroPharmacology]/SynapseTypes.png, Xe1phix-[EDU]/Xe1phix-[.NeuroPharmacology]/Chemical-Structures-of-Neurotransmitters.png, Xe1phix-[EDU]/Xe1phix-[PsychoActives-Education]/amphetamine tolerance and Ca++ influx through the NMDA receptor.pdf, Xe1phix-[EDU]/Xe1phix-[PsychoActives-Education]/TAAR1_Amphetamine_Dopamine.png, Xe1phix-[EDU]/Xe1phix-[PsychoActives-Education]/amphetamine tolerance and Ca++ influx through the NMDA receptor.txt, Xe1phix-[EDU]/Xe1phix-[PsychoActives-Education]/DrDavid-Nichol-Stuffz.txt, Xe1phix-[XFS]/Xe1phix-XFS-Cheatsheet-v4.7.sh

---
+++ Xe1phix-[EDU]/Xe1phix-[.NeuroPharmacology]/Neuropharmacology.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+#########################
+## Neuropharmacology.sh
+#########################
+
+
+
+
+
+
+
+
+
+When an action potential
+travelling along a nerve fibre reaches a nerve terminal, a
+host of ion channels open. Calcium ions enter through
+these channels into the interior of the terminal. These
+ions migrate from the channel exits to release sites on
+vesicles containing neurotransmitter molecules. A
+triggering effect of the calcium ions causes these
+contents to be dumped into the synaptic cleft that
+separates this terminal from a neighbouring neuron,
+and these neurotransmitter molecules influence the
+tendencies of that neighbouring neuron to ‘fire’.
+
+
+At their narrowest points, calcium ion channels are
+less than a nanometre in diameter (Cataldi et al. 2002).
+This extreme smallness of the opening in the calcium
+ion channels has profound quantum mechanical
+implications. The narrowness of the channel restricts
+the lateral spatial dimension. Consequently, the lateral
+velocity is forced by the quantum uncertainty principle to
+become large. This causes the quantum cloud of
+possibilities associated with the calcium ion to fan out
+over an increasing area as it moves away from the tiny
+channel to the target region where the ion will be
+absorbed as a whole, or not absorbed at all, on some
+small triggering site.
+
+This spreading of this ion wave packet means that the
+ion may or may not be absorbed on the small triggering
+site. Accordingly, the contents of the vesicle may or
+may not be released. Consequently, the quantum state
+of the brain has a part in which the neurotransmitter is
+released and a part in which the neurotransmitter is not
+released. This quantum splitting occurs at every one of
+the trillions of nerve terminals. This means that the
+quantum state of the brain splits into a vast host of
+classically conceived possibilities, one for each possible
+combination of the release-or-no-release options at
+each of the nerve terminals. In fact, because of
+uncertainties on timings and locations, what is gener-
+ated by the physical processes in the brain will be not a
+single discrete set of non-overlapping physical possibi-
+lities but rather a huge smear of classically conceived
+possibilities. 
+
+
+The Penrose–Hameroff model requires that the
+quantum state of the brain has a property called
+macroscopic quantum coherence
+
+
+
+Quantum
+computation in brain microtules: decoherence and bio-
+logical feasibility
+
+
+
+relationship between mentalistic data and neurophysio-
+logical mechanisms. Examining the qualitative and
+quantitative aspects of brain function, and doing
+detailed analyses of how they relate to the data of expe-
+rience, obtained through increasingly sophisticated
+means of psychological investigation and subject self-
+report analysis 
+
+
+ME: High-level overview Communicating with the Host OS and network
+
+
+Importance of quantum decoherence in
+brain process
+Psychology: the briefer course William James:
+writings 1879–1899
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
