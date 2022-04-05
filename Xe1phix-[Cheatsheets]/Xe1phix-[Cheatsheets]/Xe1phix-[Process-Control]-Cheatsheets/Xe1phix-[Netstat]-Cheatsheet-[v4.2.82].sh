84ea9661c3c0e4727b4c350d3a07855ff6ff356e
Xe1phix <xe1phix@protonmail.ch>
2022-04-05 23:37:40 +0000

    Update Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Process-Control]-Cheatsheets/Xe1phix-[Kill]-Cheatsheet-[v4.8.28].sh, Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Process-Control]-Cheatsheets/Xe1phix-[lsof]-Cheatsheet-[v4.6.54].sh, Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Process-Control]-Cheatsheets/Xe1phix-[Netstat]-Cheatsheet-[v4.2.82].sh

---
+++ Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Process-Control]-Cheatsheets/Xe1phix-[Netstat]-Cheatsheet-[v4.2.82].sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+
+
+
+## ------------------------------------------------------------------------------------------- ##
+    netstat -tulanp                 ## Display TCP & UDP Packets, Listening
+## ------------------------------------------------------------------------------------------- ##
+    netstat -nr                     ## Displaying The Routing Table
+## ------------------------------------------------------------------------------------------- ##
+
+
+## ------------------------------------------------------------------------------------------- ##
+    netstat -anp --tcp -4 | grep :22        ## Display Sockets Using Port 22
+## ------------------------------------------------------------------------------------------- ##
+    netstat -anp | grep DGRAM               ## Display Datagram Packets
+## ------------------------------------------------------------------------------------------- ##
+    netstat -anp | grep SEQPACKET           ## Display Sockets With Sequence Packets
+## ------------------------------------------------------------------------------------------- ##
+
+## ------------------------------------------------------------------------------------------- ##
+    netstat --statistics --raw                  ## Displaying RAW network statistics
+## ------------------------------------------------------------------------------------------- ##
+
+
+
+
+netstat -nao | find ":[port]"
+
+netstat -nao | find ":[port]" | find "[ClientIPaddr]"
+
+
+
+netstat -s | awk '/:/ { p = $1 }; (p ~ /^tcp/) { print }'
+
+
+netstat -s | awk '/:/ { p = $1 }; (p ~ /^Tcp/) { print }'
+
+
+
+
+netstat -anp --tcp -4 | awk '/:22/ && /LISTEN/ { print $7 }' | cut -f1 -d/
+
+
+netstat -anp --tcp -4 | awk '/:22/ && /LISTEN/ { print $7 }' | cut -f1 -d/
+
+
+netstat -s | awk '/:/ { p = $1 }; (p ~ /^Tcp/) { print }'
+
+
+netstat -s | awk '/:/ { p = $1 }; (p ~ /^tcp/) { print }'
+
+
+kill `netstat -anp --tcp -4 | awk '/:22/ && /LISTEN/ { print $7 }' | cut -f1 -d/`
+
+kill -9 $(ps x | grep "openvpn" | head -1 | awk {'printf $1'})
+
+
+
+
+netstat -s | grep "Total Packets Received" | awk '{print}' >> $TEMP_DIR/TotalPacketsRecieved.txt && cat -vET $TEMP_DIR/VmStatPart.txt
+netstat -s | grep "Incoming Packets Discarded" | awk '{print}' >> $TEMP_DIR/DiscardedIncomingPackets.txt && cat -vET $TEMP_DIR/DiscardedIncomingPackets.txt
+netstat -s | grep "ICMP Messages Received" | awk '{print}' >> $TEMP_DIR/ICMPMessagesReceived.txt && cat -vET $TEMP_DIR/ICMPMessagesReceived.txt
+netstat -s | grep "ICMP Messages Send" | awk '{print}' >> $TEMP_DIR/ICMPMessagesSent.txt && cat -vET $TEMP_DIR/ICMPMessagesSent.txt.txt
+netstat -s | grep "Active Connections Openings" | awk '{print}' >> $TEMP_DIR/ActiveOpenConnections.txt && cat -vET $TEMP_DIR/ActiveOpenConnections.txt
+netstat -s | grep "Passive Connections Openings" | awk '{print}' >> $TEMP_DIR/PassiveConnectionsOpenings.txt && cat -vET $TEMP_DIR/PassiveConnectionsOpenings.txt.txt
+netstat -s | grep "Connections Estabished" | awk '{print}' >> $TEMP_DIR/ConnectionsEstabished.txt && cat -vET $TEMP_DIR/ConnectionsEstabished.txt.txt
+netstat -s | grep "Bad segments Received." | awk '{print}' >> $TEMP_DIR/BadSegmentsReceived.txt && cat -vET $TEMP_DIR/BadSegmentsReceived.txt
+
+
+
+Watch port 22 and show "ESTABLISHED" connections who arent localhost
+
+sudo watch -n10 "netstat -ntu | grep :22| grep ESTAB | awk '{print \$5}' | cut -d: -f1 | grep -v 127.0.0.1 | sort"
+
+
+
+Show number of connections to port 443 (webserver)
+
+netstat -ntu | grep :443 | grep -v LISTEN | awk '{print $5}' | cut -d: -f1 | grep -v 127.0.0.1 | wc -l
+
+
+
+
+
+
+##-==============================================-##
+##    [+] pull out just the PID of the master SSH daemon:
+##-==============================================-##
+netstat -anp --tcp -4 | awk '/:22/ && /LISTEN/ { print $7 }' | cut -f1 -d/
+
+
+##-==========================================-##
+##    [+] Killing that process just requires appropriate use of backticks:
+
+kill `netstat -anp --tcp -4 | awk '/:22/ && /LISTEN/ { print $7 }' | cut -f1 -d/`
+
+
+##-==========================================-##
+##    [+] Monitor open connections for httpd
+##-==========================================-##
+##    [?] including listen, count and sort it per IP
+
+watch "netstat -plan|grep :80|awk {'print \$5'} | cut -d: -f 1 | sort | uniq -c | sort -nk 1"
+
+
