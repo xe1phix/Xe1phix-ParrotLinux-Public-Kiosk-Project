a46a9d14ae2244b341029b2c06c15d9e2ceed960
Xe1phix <xe1phix@protonmail.ch>
2022-04-06 09:18:16 +0000

    Update Xe1phix-[PIDStat]-Cheatsheet-[v2.5.24].sh

--- Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Process-Control]-Cheatsheets/Xe1phix-[PIDStat]-Cheatsheet-[v2.5.24].sh
+++ Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Process-Control]-Cheatsheets/Xe1phix-[PIDStat]-Cheatsheet-[v2.5.24].sh
@@ -2,24 +2,24 @@
 
 
 ## -------------------------------------------------------------------- ##
-      pidstat -r			## Report page faults
+      pidstat -r              ## Report page faults
 ## -------------------------------------------------------------------- ##
       pidstat -s			## Stack utilization
 ## -------------------------------------------------------------------- ##
       pidstat -d			## Report  I/O statistics
 ## -------------------------------------------------------------------- ##
-      pidstat -p $PID       ## Resource consumption details
+      pidstat -p $PID         ## Resource consumption details
 ## -------------------------------------------------------------------- ##
-      pidstat -p ALL        ## Resource consumption of all processes
+      pidstat -p ALL          ## Resource consumption of all processes
 ## -------------------------------------------------------------------- ##
-      pidstat -u            ## Report CPU utilization
+      pidstat -u              ## Report CPU utilization
 ## -------------------------------------------------------------------- ##
-      pidstat -l            ## Display the process command
+      pidstat -l              ## Display the process command
 ## -------------------------------------------------------------------- ##
 
 
 ##-==========================================================-##
-##  [+] Display  global  page faults and memory statistics
+##  [+] Display global page faults and memory statistics
 ##      for processes firefox & chrome
 ##-==========================================================-##
 pidstat -C "firefox|chrome" -r -p ALL
