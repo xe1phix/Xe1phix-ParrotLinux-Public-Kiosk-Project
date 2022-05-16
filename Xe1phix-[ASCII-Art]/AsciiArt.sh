aa1e4b7c825161b11f3bfe654bffca9123b65349
Xe1phix <xe1phix@protonmail.ch>
2022-05-16 10:01:15 +0000

    Update Xe1phix-[ASCII-Art]/Xe1phix-PCI-Slots-ASCII-Art.png, Xe1phix-[ASCII-Art]/Xe1phix-PCI-Slots-ASCII-Art.txt, Xe1phix-[ASCII-Art]/[Community]-ASCII-Art/docker-vpnbox-Graph-ASCII-Art.txt, Xe1phix-[ASCII-Art]/[Community]-ASCII-Art/Activism-Mo-Golden-Keys-ASCII-Art.md, Xe1phix-[ASCII-Art]/[Community]-ASCII-Art/[Iniciantes]-[Buffer-Overflow-Tutorial]-ASCII-Art.txt, Xe1phix-[ASCII-Art]/[Community]-ASCII-Art/ZeroConf-Tinc-VPN-Welcome-ASCII-Art.txt, Xe1phix-[ASCII-Art]/[Community]-ASCII-Art/VX-Underground-[Ascii-Art].txt, Xe1phix-[ASCII-Art]/[Community]-ASCII-Art/VX-Underground-[Ascii-Art], Xe1phix-[ASCII-Art]/toilet Mr self distruct diamonds.png, Xe1phix-[ASCII-Art]/toilet Mr self distruct diamonds, Xe1phix-[ASCII-Art]/AsciiArt.sh, Xe1phix-[ASCII-Art]/Chmod-Attributes-Cheatsheet.png, Xe1phix-[ASCII-Art]/Chmod-Cheatsheet.png, Xe1phix-[ASCII-Art]/UMask-Cheatsheet.png, Xe1phix-[ASCII-Art]/Chmod-Modes-Descriptions-Table.png, Xe1phix-[ASCII-Art]/Xe1phix-[I2P]-Overview-of-[Protocol-Specs]>x<[I2P]-Carefully-Crafted-Anonymous-[Protocol-Stack]+[ASCII-Art]-Analysis-of-I2Ps-[Packet-Headers]-(ReFormatted)-[v8.4.5]-(05-07-21)-.txt

---
+++ Xe1phix-[ASCII-Art]/AsciiArt.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+## AsciiArt.sh
+
+toilet Xe1phix -f ivrit | boxes -d html | boxes -d html | boxes -d html | boxes -d html | boxes -d html | boxes -d html | boxes -d html | boxes -d html
+
+
+cowsay -W 28 -f sodomized-sheep "From Nothing To Being, There is No Logical Bridge."
+
+
+
---
+++ Xe1phix-[ASCII-Art]/[Community]-ASCII-Art/Activism-Mo-Golden-Keys-ASCII-Art.md
@@ -0,0 +1,12 @@
+```
+ ___________
+|
+| just say no 
+| to weakened
+|+ backdoored
+| crypto 🔑
+|___________
+(\__/) || 
+(•ㅅ•) || 
+/ 　 づ ‪#‎nogoldenkeys‬
+```
