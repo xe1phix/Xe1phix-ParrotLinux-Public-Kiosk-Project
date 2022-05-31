c23e9fe9a76908dabe580d7c58c472f0bdadcc82
Xe1phix <xe1phix@protonmail.ch>
2022-05-31 21:57:47 +0000

    Update Xe1phix-[Netstat]-Cheatsheet-[v4.2.82].sh

--- Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Process-Control]-Cheatsheets/Xe1phix-[Netstat]-Cheatsheet-[v4.2.82].sh
+++ Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Process-Control]-Cheatsheets/Xe1phix-[Netstat]-Cheatsheet-[v4.2.82].sh
@@ -24,6 +24,47 @@
 
 
 
+##-===============================================================-##
+##   [+] Show tcp connections sorted by Host / Most connections
+##-===============================================================-##
+netstat -ntu|awk '{print $5}'|cut -d: -f1 -s|sort|uniq -c|sort -nk1 -r
+
+
+##-==============================================================-##
+##   [+] Summarize the number of open TCP connections by state
+##-==============================================================-##
+netstat -nt | awk '{print $6}' | sort | uniq -c | sort -n -k 1 -r
+
+
+##-====================================================-##
+##   [+] Monitor open connections for httpd 
+##       including listen, count and sort it per IP
+##-====================================================-##
+watch "netstat -plan|grep :80|awk {'print \$5'} | cut -d: -f 1 | sort | uniq -c | sort -nk 1"
+
+
+##-=====================================================================================-##
+##   [+] List all active access_logs for currently running Apache or Lighttpd process
+##-=====================================================================================-##
+lsof -p $(netstat -ltpn|awk '$4 ~ /:80$/ {print substr($7,1,index($7,"/")-1)}| awk '$9 ~ /access.log$/ {print $9| "sort -u"}'
+
+
+##-=======================================================================-##
+##   [+] List top 20 IP from which TCP connection is in SYN_RECV state
+##-=======================================================================-##
+netstat -pant 2> /dev/null | grep SYN_ | awk '{print $5;}' | cut -d: -f1 | sort | uniq -c | sort -n | tail -20
+
+
+##-=============================================================================-##
+##   [+] obtain a list of geographic localization for established connections
+##-=============================================================================-##
+for i in $(netstat --inet -n|grep ESTA|awk '{print $5}'|cut -d: -f1);do geoiplookup $i;done
+
+
+
+
+
+
 netstat -nao | find ":[port]"
 
 netstat -nao | find ":[port]" | find "[ClientIPaddr]"
@@ -83,21 +124,21 @@ netstat -ntu | grep :443 | grep -v LISTEN | awk '{print $5}' | cut -d: -f1 | gre
 
 
 
-##-==============================================-##
+##-=========================================================-##
 ##    [+] pull out just the PID of the master SSH daemon:
-##-==============================================-##
+##-=========================================================-##
 netstat -anp --tcp -4 | awk '/:22/ && /LISTEN/ { print $7 }' | cut -f1 -d/
 
 
-##-==========================================-##
+##-==========================================================================-##
 ##    [+] Killing that process just requires appropriate use of backticks:
-
+##-==========================================================================-##
 kill `netstat -anp --tcp -4 | awk '/:22/ && /LISTEN/ { print $7 }' | cut -f1 -d/`
 
 
-##-==========================================-##
+##-============================================-##
 ##    [+] Monitor open connections for httpd
-##-==========================================-##
+##-===========================================-##
 ##    [?] including listen, count and sort it per IP
 
 watch "netstat -plan|grep :80|awk {'print \$5'} | cut -d: -f 1 | sort | uniq -c | sort -nk 1"
