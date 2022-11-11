e56eb8db9cd062848c1722efbcf0cc7791c40808
Xe1phix <xe1phix@protonmail.ch>
2022-11-11 00:35:06 +0000

    Update Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Virtualization]-Cheatsheets/Xe1phix-[KVM]-[Qemu]-Converting-Virtual-Disk-Images-Commandline-Fu-[Cheatsheet]-Table-[v4.5.24].sh, Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Virtualization]-Cheatsheets/Xe1phix-[QCow2]-Cheatsheet-[v2.5.85].sh, Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Virtualization]-Cheatsheets/qemu-img-cheatsheet.png, Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[FileSystem]-Cheatsheets/Xe1phix-[QCow2]-Cheatsheet/Xe1phix-[QCow2]-Cheatsheet-[v2.5.85].sh

---
+++ Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[FileSystem]-Cheatsheets/Xe1phix-[QCow2]-Cheatsheet/Xe1phix-[QCow2]-Cheatsheet-[v2.5.85].sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+
+##-==========================================-##
+##   [+] Create a QCow2 image from a HDD
+##-==========================================-##
+e2image -Q /dev/sda1 $Image.qcow2
+
+
+##-================================================-##
+##   [+] Convert a QCow2 image into a Raw image
+##-================================================-##
+e2image -r $Image.qcow2 $Image.raw
+
+
+##-=================================================-##
+##   [+] Mount A QCOW2 Image:
+##-=================================================-##
+qcowmount $File.qcow2 /mnt/qcowimage/
+
+
+##-=================================================-##
+##   [+] Pass "allow_root" to the fuse subsystem:
+##-=================================================-##
+qcowmount -X allow_root $File.qcow2 /mnt/qcowimage/
+
+
+##-=================================================-##
+##   [+] Unmount /mnt/qcowimage/ Using umount:
+##-=================================================-##
+unmount /mnt/qcowimage/
+
+
+##-=================================================-##
+##   [+] Unmount /mnt/qcowimage/ Using fusermount:
+##-=================================================-##
+fusermount -u /mnt/qcowimage/
+
+
+##-========================================================================-##
+##   [+] Create a new image, read/write snapshot of the original image:
+##-========================================================================-##
+qemu-img create -f qcow2 -b $File.img snapshot.img
+
+
+##-==========================================-##
+##   [+] Determine an images backing file
+##-==========================================-##
+qemu-img info $File.img
+
+
+##-=================================================================-##
+##                  [+] Temporary snapshots
+##-=================================================================-##
+## ----------------------------------------------------------------- ##
+##   [?] any changes made to the virtual machine while it is 
+##       running are written to temporary files and thrown away 
+##       when the virtual machine is turned off
+## ----------------------------------------------------------------- ##
+qemu -hda $File.img -snapshot
+
