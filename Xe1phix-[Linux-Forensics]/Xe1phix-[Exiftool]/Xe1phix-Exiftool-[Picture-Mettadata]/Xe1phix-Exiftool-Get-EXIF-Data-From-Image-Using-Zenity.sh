131ee7a9c56b3da250448702f9d3acf0e5679b6c
Xe1phix <xe1phix@protonmail.ch>
2022-12-03 21:55:58 +0000

    Deleted Xe1phix-[Linux-Forensics]/Xe1phix-[Exiftool]/Xe1phix-Exiftool-[Picture-Mettadata]/Xe1phix-Exiftool-Find-Corrupt-Jpeg-Files.sh, Xe1phix-[Linux-Forensics]/Xe1phix-[Exiftool]/Xe1phix-Exiftool-[Picture-Mettadata]/Xe1phix-Exiftool-GMaps-URL-From-Picture-Metadata.sh, Xe1phix-[Linux-Forensics]/Xe1phix-[Exiftool]/Xe1phix-Exiftool-[Picture-Mettadata]/Xe1phix-Exiftool-Get-EXIF-Data-From-Image-Using-Zenity.sh

--- Xe1phix-[Linux-Forensics]/Xe1phix-[Exiftool]/Xe1phix-Exiftool-[Picture-Mettadata]/Xe1phix-Exiftool-Get-EXIF-Data-From-Image-Using-Zenity.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-##-=========================================-##
-## [+] Xe1phix-[Exiftool]-Get-[EXIF]-Data-From-[Image]-Using-[Zenity]-[v2.4.58].sh
-##-=========================================-##
-## ------------------------------------------------------------------ ##
-##  [?] Get EXIF data from image with zenity
-## ------------------------------------------------------------------ ##
-ans=$(zenity --title "Choose image:" --file-selection); exiftool -s ${ans} | zenity --width 800 --height 600 --text-info;
