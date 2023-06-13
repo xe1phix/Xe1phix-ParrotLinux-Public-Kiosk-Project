c28594466364af43f344fb986d2807f0265464b9
Xe1phix <xe1phix@protonmail.ch>
2023-06-13 18:38:07 +0000

    Update 34 files
    - /Xe1phix-[Cheatsheets]/Community-[Cheatsheets]/[Pentesting]-Cheatsheets/The-Redteam-Guide.pdf
    - /Xe1phix-[Cheatsheets]/Xe1phix-[OpenSSL]+[GnuTLS]-Cheatsheet-[v14.8.54.].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-GnuTLS-Serv-Create-Secure-Encrypted-Connections-Using-GnuTLS-[5.8.42].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/openssl s_client.jpg
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[PS]-Cheatsheet-[v5.4.84].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-PCAP-CLI-Cheatsheet-[v4.2.54].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-PCAP-CLI-Cheatsheet.png
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/pgrep+pidstat.jpg
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Network-Forensics]-Cheatsheets/TCPDump-Cheatsheet-Large.png
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[DFIR]-[PDF-Analysis]-Cheatsheet-[v24.5.64].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Exiftool]+[Exiv2]+[Exifprobe]-Metadata-Manipulation-Cheatsheet-[v28.4.62].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Kernel-Compilation]-Cheatsheet.png
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-fping-Cheatsheet.jpg
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Fsck]-Cheatsheet-[v4.5.92].sh
    - /Xe1phix-[Cheatsheets]/Top-[Cheatsheets]/system-hardening-10.2.txt
    - /Xe1phix-[Cheatsheets]/Top-[Cheatsheets]/system-hardening-13.1.txt
    - /Xe1phix-[Cheatsheets]/Community-[Cheatsheets]/[Process-Admin]-Cheatsheets/Incident-Response-Reference.pdf
    - /Xe1phix-[Cheatsheets]/Top-[Cheatsheets]/[Forensics-Analysis]-Cheatsheets/Autopsy-Digital-Forensics-Guide.pdf
    - /Xe1phix-[Cheatsheets]/Top-[Cheatsheets]/[Forensics-Analysis]-Cheatsheets/LinuxLeo-4.96.pdf
    - /Xe1phix-[Cheatsheets]/Top-[Cheatsheets]/Top-[Hardening]-Cheatsheets/system-hardening-10.2.txt
    - /Xe1phix-[Cheatsheets]/Top-[Cheatsheets]/Top-[Hardening]-Cheatsheets/system-hardening-13.1.txt
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[OpenSSL]-Cheatsheets/openssl s_client.jpg
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[OpenSSL]-Cheatsheets/Xe1phix-[OpenSSL]+[GnuTLS]-Cheatsheet-[v14.8.54.].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[PS]-Cheatsheets/Xe1phix-[PS]-Cheatsheet-[v5.4.84].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Network-Forensics]-Cheatsheets/Xe1phix-PCAP-CLI-Cheatsheet-[v4.2.54].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Network-Forensics]-Cheatsheets/Xe1phix-PCAP-CLI-Cheatsheet.png
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Network-Forensics]-Cheatsheets/Xe1phix-[TCPDump]-Cheatsheets/TCPDump-Cheatsheet-Large.png
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Process-Control]-Cheatsheets/pgrep+pidstat.jpg
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Forensic-Analysis]-Cheatsheets/Xe1phix-[DFIR]-[PDF-Analysis]-Cheatsheet-[v24.5.64].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Forensic-Analysis]-Cheatsheets/Xe1phix-[Exiftool]+[Exiv2]+[Exifprobe]-Metadata-Manipulation-Cheatsheet-[v28.4.62].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Kernel]-Cheatsheets/Xe1phix-[Kernel-Compilation]-Cheatsheet.png
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[GnuTLS]-Cheatsheets/Xe1phix-GnuTLS-Serv-Create-Secure-Encrypted-Connections-Using-GnuTLS-[5.8.42].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Red-Teaming]-Cheatsheets/Xe1phix-fping-Cheatsheet.jpg
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[DFIR]-Digital-Forensics-And-Incident-Response-Cheatsheets/Xe1phix-[Fsck]-Cheatsheet-[v4.5.92].sh

---
+++ Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[PS]-Cheatsheets/Xe1phix-[PS]-Cheatsheet-[v5.4.84].sh
@@ -0,0 +1,224 @@
+#!/bin/sh
+
+
+##-===========================================-##
+##   [+] show top 10 process eating memory
+##-===========================================-##
+ps auxf | sort -nr -k 4 | head -10
+
+
+##-=========================================-##
+##   [+] show top 10 process eating CPU
+##-=========================================-##
+ps auxf | sort -nr -k 3 | head -10
+
+
+##-===========================================-##
+##   [+] Show Every Process Running As Root
+##-===========================================-##
+ps -U root -u root u
+
+
+##-========================================-##
+##   [+] List All Threads For A process:
+##-========================================-##
+ps -C firefox -L -o pid,tid,pcpu,state
+
+
+##-=================================================-##
+##   [+] Print User, Service And Num of Processes
+##-=================================================-##
+ps -ef | awk '{print $1}' | sort | uniq -c | sort -nr
+
+
+##-=========================================-##
+##   [+] Print The Process ID of Rsyslogd
+##-=========================================-##
+ps -C rsyslogd -o pid
+
+
+##-===============================-##
+##   [+] Print sshd Daemon PID
+##-===============================-##
+ps -ef | awk '/sshd/ {print $2}'
+
+
+##-=========================================-##
+##   [+] Find The sshd Daemon, And Kill It
+##-=========================================-##
+kill $(ps -ef | awk '/sshd/ {print $2}')
+
+
+##-=============================================-##
+##   [+] PSTree - Graphical list of processes
+##-=============================================-##
+pstree --arguments --show-pids --show-pgids --show-parents
+
+
+##-=================================================-##
+##   [+] Print User, Service And Num of Processes
+##-=================================================-##
+ps -ef | awk '{print $1}' | sort | uniq -c | sort -nr
+
+
+##-========================================-##
+##   [+] List All Threads For A process:
+##-========================================-##
+ps -C $Service -L -o pid,tid,pcpu,state
+
+
+##-===============================-##
+##   [+] SSH Uptime Statistics:
+##-===============================-##
+ps -eo pid,cmd,etime | egrep -E 'ssh-agent|gnome-keyring-daemon'
+
+
+##-=======================================-##
+##   [+] List processes in a hierarchy
+##-=======================================-##
+ps -e -o pid,args --forest
+
+
+##-=========================================-##
+##   [+] Find Processes Being Run As Root
+##-=========================================-##
+ps -ef | awk '$1 == "root" && $6 != "?" {print}'
+
+
+##-=================================-##
+##   [+] Indepth Group Statistics
+##-=================================-##
+ps -eo pid,command,size,vsize,%mem,gid,sgid,egid,fgid,sgroup,rgroup,group,fgroup,egroup,tpgid,tgid,flags
+
+
+##-================================================-##
+##   [+] Stack Statistics, esp eip nwchan etc...
+##-================================================-##
+ps -eo pid,uid,user,command,vsize,esp,eip,stackp,nwchan,lwp,psr,nlwp,flags
+
+
+##-=========================================-##
+##   [+] Verbose User & UID Statistics
+##-=========================================-##
+ps -eo uid,fuid,suid,ruid,euid,fuser,suser,user,uname,ruser,euser,command
+
+
+##-======================================-##
+##   [+] Verbose Mem & Cpu Statistics
+##-======================================-##
+ps -eo uid,gid,user,group,command,size,vsize,sz,%mem,%cpu,flags
+
+
+##-=======================================================-##
+##   [+] pull out just the PID of the master SSH daemon:
+##-=======================================================-##
+netstat -anp --tcp -4 | awk '/:22/ && /LISTEN/ { print $7 }' | cut -f1 -d/
+
+
+##-=================================================-##
+##   [+] Count processes related to HTTP server
+##-=================================================-##
+ps aux | grep http | grep -v grep | wc -l
+
+
+##-=================================================-##
+##   [+] Display top 5 processes consuming CPU
+##-=================================================-##
+ps -eo pcpu,user,pid,cmd | sort -r | head -5
+
+
+##-========================================================================-##
+##   [+] Display the top ten running processes - sorted by memory usage
+##-========================================================================-##
+ps aux | sort -nk +4 | tail
+
+
+##-==================================-##
+##   [+] Print the PID of Chrome:
+##-==================================-##
+chrome_pid=$(ps -aux | grep "[c]hrome --user-data" | awk '{ print $2 }' | head -n 1 2>/dev/null)
+
+
+##-===============================-##
+##   [+] Kill Chrome processes
+##-===============================-##
+alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
+
+
+##-=============================================================================-##
+##   [+] Use STrace to attach to all of the currently running apache processes
+##-=============================================================================-##
+ps auxw | grep sbin/apache | awk '{print"-p " $2}' | xargs strace
+
+pgrep -f /usr/sbin/httpd | awk '{print"-p " $1}' | xargs strace
+
+ps auxw | grep -E 'sbin/(apache|httpd)' | awk '{print"-p " $2}' | xargs strace -F
+
+
+##-===========================================================-##
+##   [+] Use STrace to attach to all apache child processes
+##-===========================================================-##
+ps h --ppid $(cat /var/run/apache2.pid) | awk '{print"-p " $1}' | xargs sudo strace
+
+
+##-=========================================-##
+##    [+] Kill all processes of $Service
+##-=========================================-##
+kill -9 $(ps aux | grep '$Service' | awk '{print $2}')
+
+
+##-===================================-##
+##     [+] Kill all Zombie processes
+##-===================================-##
+kill -9 `ps -xaw -o state -o ppid | grep Z | grep -v PID | awk '{print $2}'`
+
+
+
+## ----------------------------------------------------------------------------------------- ##
+	kill $(ps -ef | awk '/sshd/ {print $2}')		## kill sshd Processes
+## ----------------------------------------------------------------------------------------- ##
+	kill $(ps -ef | awk '/apache2/ {print $2}')		## kill Apache2 Processes
+## ----------------------------------------------------------------------------------------- ##
+	kill $(ps -ef | awk '/mysql/ {print $2}')		## kill mysql Processes
+## ----------------------------------------------------------------------------------------- ##
+
+
+##-==============================================-##
+##   [+] Display any tcp connections to apache
+##-==============================================-##
+for i in `ps aux | grep httpd | awk '{print $2}'`; do lsof -n -p $i | grep ESTABLISHED; done;
+
+
+##-==============================================================================================-##
+##   [+] Show top 50 running processes ordered by highest memory/cpu usage refreshing every 1s
+##-==============================================================================================-##
+watch -n1 "ps aux --sort=-%mem,-%cpu | head -n 50"
+
+
+##-=================================================-##
+##   [+] Count processes related to HTTP server
+##-=================================================-##
+ps aux | grep http | grep -v grep | wc -l
+
+
+
+
+
+
+(ps -aux; ps -ejH; ps -eLf; ps axjf; ps axms; ps -ely; ps -ef; ps -eF;  ps -U root -u root u; ) > ps-dump.txt
+(ps -eo 'pid,user,group,nice,vsz,rss,comm') > ps-table-dump.txt
+
+ps aox 'pid,user,args,size,pcpu,pmem,pgid,ppid,psr,tty,session,eip,esp,start_time' > ps-columns.txt
+
+
+ps -eo pid,user,args --sort user
+
+ps -eo pid,user,group,args,etime,lstart jgrep $PID
+
+ps -eo pid,user,group,gid,vsz,rss,comm --sort=-rss | less
+
+ps -ef --sort=user | less
+
+List Processes By % Cpu Usage
+ps -e -o pcpu,cpu,nice,state,cputime,args --sort pcpu | sed '/^ 0.0 /d'
+
