acb94bf04c7aa4bec429acb7a3c7b6a405c3ed48
Xe1phix <xe1phix@protonmail.ch>
2024-02-14 01:38:46 +0000

    Update 18 files
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[OSINT]-Cheatsheets/Xe1phix-[FinalRecon]-Cheatsheet-[v4.8.92].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[OSINT]-Cheatsheets/Xe1phix-[Pentest+]-Syntax-Notes-[v48.5.48].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-[bbot]-Cheatsheet-[v4.5.48].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-[CMS]-[Vulnerability-Scanners]-Cheatsheet-[v5.8.92].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-[Curl]-Pentesting-CLI-Cheatsheet-[v4.8.52].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-[DotDotPwn]-Cheatsheet-[v5.9.54].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-[Feroxbuster]-Cheatsheet-[v5.4.92].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-[FinalRecon]-Cheatsheet-[v4.8.92].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-[Fuzzing]-Cheatsheet-v12.4.82.sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-[Joomscan]-Cheatsheet-[v2.5.82].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-[Kerberos]-Pentesting-Cheatsheet-[v4.8.92].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-[ParamSpider]-Cheatsheet-[v4.5.82].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-[Pyrit]-Cheatsheet-[v4.5.82].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-[RDPChecker]-Cheatsheet-[v4.8.52].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-[Silver]-Cheatsheet-[v4.5.28].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-[SQLi]-Pentesting-CLI-Syntax-Cheatsheet-[v5.4.82].sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-Web-Fuzzing-Cheatsheet-v5.2.84.sh
    - /Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-[Pentest+]-Syntax-Notes-[v48.5.48].sh

---
+++ Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-[Joomscan]-Cheatsheet-[v2.5.82].sh
@@ -0,0 +1,13 @@
+
+
+----
+# Joomscan (joomla)
+Do default checks
+
+perl joomscan.pl -u www.example.com
+
+
+
+Enumerate installed components
+
+perl joomscan.pl -u www.example.com --ec
---
+++ Xe1phix-[Cheatsheets]/Xe1phix-[Cheatsheets]/Xe1phix-[Pentesting]-Cheatsheets/Xe1phix-[Pyrit]-Cheatsheet-[v4.5.82].sh
@@ -0,0 +1,17 @@
+
+
+----
+##  [+]  Pyrit-Cheatsheet.md
+
+
+##  [+]  Pyrit - DB attacks
+
+pyrit eval
+pyrit -i (wordlist) import_passwords
+pyrit -e (essid) create_essid
+pyrit batch
+pyrit batch -r (capturefile) -b(AP MAC) attack_db
+pyrit strip
+pyrit -r (capturefile) -o (capturefile output) strip
+pyrit dictionary attack
+pyrit -r (capturefile) -i (/pathtowordlist) -b (AP MAC) attack_passthrough
